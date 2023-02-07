2fab739ced2efa475925214971bed7d5a1b47d24
Leandro Morilha <lmorilha@ciandt.com>
2023-02-07 10:36:37 -0300

    pipeline template config

---
+++ global_pipeline_configuration/pipeline_templates/teste01/pipeline_config.groovy
@@ -0,0 +1,17 @@
+
+@merge libraries{
+    sonnarqube
+    splunk{
+        afterSteps = [ "static_code_analysis", "unit_test"  ]
+    }
+}
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
+}
