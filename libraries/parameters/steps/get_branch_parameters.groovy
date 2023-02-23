c5ff7d5c866985634e3a6c2011768e855bb30ba6
Leandro Morilha <lmorilha@ciandt.com>
2023-02-23 15:42:10 -0300

    teste

--- libraries/parameters/steps/get_branch_parameters.groovy
+++
@@ -1,7 +0,0 @@
-package parameters
-
-class BranchParameters implements Serializable {
-    static Map get(Map branchParameters, String branch) {
-        return branchParameters.get(branch, branchParameters.get('desenvolvimento'))
-    }
-}
