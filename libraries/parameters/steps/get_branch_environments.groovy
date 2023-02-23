9ee6065c5645f7a0e5f5dc9e023e9ab73cfc4f4a
Leandro Morilha <lmorilha@ciandt.com>
2023-02-23 15:46:36 -0300

    teste

--- libraries/parameters/steps/get_branch_environments.groovy
+++ libraries/parameters/steps/get_branch_environments.groovy
@@ -4,19 +4,39 @@
 * @param branchName the name of the branch to get configuration
 * @return (Map) returns a map containing the branch configurations comming from pipeline's pipeline_config.groovy
 */
+import hudson.EnvVars;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.NodeProperty;
+import hudson.slaves.NodePropertyDescriptor;
+import hudson.util.DescribableList;
+import jenkins.model.Jenkins;
+public createGlobalEnvironmentVariables(String key, String value){
+
+       Jenkins instance = Jenkins.getInstance();
+
+       DescribableList<NodeProperty<?>, NodePropertyDescriptor> globalNodeProperties = instance.getGlobalNodeProperties();
+       List<EnvironmentVariablesNodeProperty> envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class);
+
+       EnvironmentVariablesNodeProperty newEnvVarsNodeProperty = null;
+       EnvVars envVars = null;
+
+       if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+           newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+           globalNodeProperties.add(newEnvVarsNodeProperty);
+           envVars = newEnvVarsNodeProperty.getEnvVars();
+       } else {
+           envVars = envVarsNodePropertyList.get(0).getEnvVars();
+       }
+       envVars.put(key, value)
+       instance.save()
+}
+
 void call(String branchName) {
   def environments = branch_parameters.get(branch_parameters.desenvolvimento, branch_parameters.desenvolvimento)
 
     environments.each {
-        key, value -> def env.key = value
+        key, value -> createGlobalEnvironmentVariables(key,value)
     }
 }
 
-        
-        
 
-        //environments.each {
-       //     key, value -> def env.key = value
-       // }
-
-        //return branchEnvironments.get(branch, branchEnvironments.get('desenvolvimento'))
